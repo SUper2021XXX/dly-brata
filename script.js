2fb4d908a35af8259c92433f052c4e31967ca89b
toni25010 <supergeroy11@gmail.com>
2022-12-16 19:22:21 +0300

    u

--- script.js
+++ script.js
@@ -6,13 +6,7 @@ import * as cordova from "https://cdn.skypack.dev/cordova@11.0.0";
 
           function start() {
             return window.screen.orientation.lock('landscape');
-          }
+          };
           function message() {
             return alert('test start');
-          }
-
-
-
-
-
-alert('Orientation is ' + window.screen.orientation.type);
+          };
