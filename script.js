88edd579c676ce10508e898c5630f9be5e34beaf
toni25010 <supergeroy11@gmail.com>
2022-12-16 18:59:24 +0300

    update i4.html and script.js

--- script.js
+++ script.js
@@ -1,34 +1,19 @@
-const log = document.getElementById("log");
+import * as cordova from "https://cdn.skypack.dev/cordova@11.0.0";
 
-// Lock button: Lock the screen to the other orientation (rotated by 90 degrees)
-const rotate_btn = document.querySelector('#lock_button');
-rotate_btn.addEventListener('click', () => {
-  log.textContent+=`Lock pressed \n`;
 
-  const oppositeOrientation = screen.orientation.type.startsWith("portrait") ? "landscape" : "portrait";
-  screen.orientation.lock(oppositeOrientation)
-    .then(() => {
-      log.textContent = `Locked to ${oppositeOrientation}\n`
-      }
-    )
-    .catch((error) => {
-      log.textContent += `${error}\n`;
-    });
-});
+function getAccel(){
+  DeviceMotionEvent.requestPermission().then(response => {
+      if (response == 'granted') {
 
-// Unlock button: Unlock the screen orientation (if locked)
-const unlock_btn = document.querySelector('#unlock_button');
-unlock_btn.addEventListener('click', () => {
-  log.textContent+='Unlock pressed \n';
-  screen.orientation.unlock();
-} );
 
-// Full screen button: Set the example to fullscreen.
-const fullscreen_btn = document.querySelector('#fullscreen_button');
-fullscreen_btn.addEventListener('click', () => {
-  log.textContent+='Fullscreen pressed \n';
-  const container = document.querySelector("#example_container");
-  container.requestFullscreen().catch((error) => {
-      log.textContent += `${error}\n`
+          function start () {
+              return window.screen.orientation.lock('landscape');
+          }
+
+
+
+}
   });
-} );
+}
+
+alert('Orientation is ' + window.screen.orientation.type);
